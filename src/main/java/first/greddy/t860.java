06a8ca4814902e8d8c237b0ad28199590c7270a9
tanjiaming99 <tanjiaming99@163.com>
2022-01-17 23:18:28 +0800

    【easy】贪心：柠檬水找零

---
+++ src/main/java/first/greddy/t860.java
@@ -0,0 +1,29 @@
+package first.greddy;
+
+public class t860 {
+    public boolean lemonadeChange(int[] bills) {
+        // 20块不用找零，所以可以不用记录
+        int c5 = 0, c10 = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                c5++;
+            } else if (bill == 10) {
+                c10++;
+                c5--;
+            } else if (bill == 20) {
+                // 情况想漏了，找三张五块或者一张十块一张五块
+                // 贪心！优先找10+5，因为5更万能。
+                if (c10 > 0) {
+                    c5--;
+                    c10--;
+                } else {
+                    c5 -= 3;
+                }
+            }
+            if (c5 < 0 || c10 < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
