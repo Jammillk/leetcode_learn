85c69239a75530320a686b3066a167c10d20ac06
tanjiaming99 <tanjiaming99@163.com>
2022-01-18 19:37:50 +0800

    【middle】贪心：763. 划分字母区间
    好像也不是贪心，不过确实挺小巧的一道题。

--- src/main/java/first/greddy/t435.java
+++ src/main/java/first/greddy/t435.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 
 public class t435 {
     public int eraseOverlapIntervals(int[][] intervals) {
+        // 按区间右侧进行排序
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
         int count = 1;
         int right = intervals[0][1];
@@ -19,6 +20,7 @@ public class t435 {
 
 
     public int eraseOverlapIntervals2(int[][] intervals) {
+        // 按区间左边界进行排序
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
         int count = 1;
         for (int i = 1; i < intervals.length; i++) {
