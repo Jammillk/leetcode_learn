1a3882fc4d050afcf3388377eb0168808fa5b125
tanjiaming99 <tanjiaming99@163.com>
2022-01-21 00:09:32 +0800

    【easy】动态规划：70.爬楼梯

---
+++ src/main/java/first/dp/t70.java
@@ -0,0 +1,17 @@
+package first.dp;
+
+public class t70 {
+
+    public int climbStairs2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
