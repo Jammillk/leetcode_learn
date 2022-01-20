ed3678dc0661f28b92b4bb1edfd88fb7c079bdb7
tanjiaming99 <tanjiaming99@163.com>
2022-01-20 23:36:14 +0800

    【easy】动态规划：509. 斐波那契数

---
+++ src/main/java/first/dp/t509.java
@@ -0,0 +1,20 @@
+package first.dp;
+
+public class t509 {
+    public static void main(String[] args) {
+        t509 t509 = new t509();
+        System.out.println(t509.fib(5));
+    }
+    public int fib(int n) {
+        if  (n <= 1){
+            return n;
+        }
+        int first = 0, second = 1;
+        for(int i = 2;i <= n;i++){
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+        return second;
+    }
+}
