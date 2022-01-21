ffb164c43ee22ff80801565ba47b81d1cbe545da
tanjiaming99 <tanjiaming99@163.com>
2022-01-21 17:38:19 +0800

    【middle】动态规划：96. 不同的二叉搜索树 ???

---
+++ src/main/java/first/dp/Main.java
@@ -0,0 +1,12 @@
+package first.dp;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        a(main);
+        System.out.println(main);
+    }
+    public static void a(Main main){
+        main = null;
+    }
+}
