ed5ed33d2e68d0d7be62ec4f76c3fcbe11c7e3be
tanjiaming99 <tanjiaming99@163.com>
2022-01-27 23:36:42 +0800

    【easy】动态规划：674. 最长连续递增序列

---
+++ src/main/java/first/dp/t674.java
@@ -0,0 +1,21 @@
+package first.dp;
+
+import java.util.Arrays;
+
+public class t674 {
+    public int findLengthOfLCIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] > nums[i]) {
+                dp[i + 1] = dp[i] + 1;
+            }
+            if (dp[i + 1] > res) {
+                res = dp[i + 1];
+            }
+        }
+        return res;
+    }
+}
