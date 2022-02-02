b1e898830e0e8defaf722622447737fc4b7f4ed3
tanjiaming99 <tanjiaming99@163.com>
2022-02-02 23:24:42 +0800

    【easy】二叉树：144：迭代前序遍历

---
+++ src/main/java/first/tree/PreOrder.java
@@ -0,0 +1,28 @@
+package first.tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class PreOrder {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null){
+            return list;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null){
+                stack.push(node.right);
+            }
+            if (node.left != null){
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+}
