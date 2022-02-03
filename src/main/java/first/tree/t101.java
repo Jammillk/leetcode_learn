b229614ff12412843df305757cb26334c86eacf7
tanjiaming99 <tanjiaming99@163.com>
2022-02-03 17:01:40 +0800

    【middle】二叉树：101. 对称二叉树

---
+++ src/main/java/first/tree/t101.java
@@ -0,0 +1,20 @@
+package first.tree;
+
+public class t101 {
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root, root);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null && right != null || left != null && right == null) {
+            return false;
+        }
+        if (left.val != right.val){
+            return false;
+        }
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+}
--- src/main/java/first/tree/t226.java
+++ src/main/java/first/tree/t226.java
@@ -26,6 +26,23 @@ public class t226 {
         }
         return root;
     }
+    public static void main(String[] args) {
+        t226 obj = new t226();
+        TreeNode root4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node9 = new TreeNode(9);
+        root4.left = node2;
+        root4.right = node7;
+        node2.left = node1;
+        node2.right = node3;
+        node7.left = node6;
+        node7.right = node9;
+        obj.invertTree2(root4);
+    }
 
     public TreeNode invertTree2(TreeNode root) {
         if (root == null) {
@@ -33,10 +50,11 @@ public class t226 {
         }
         // 前后序遍历都可以
         invertTree2(root.left);
-        invertTree2(root.right);
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
+        // 注：中序会翻两次！
+        invertTree2(root.right);
         return root;
     }
 }
