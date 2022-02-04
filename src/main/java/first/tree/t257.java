b769a0e675667e3e147ff957b77eb65dcd8f080e
tanjiaming99 <tanjiaming99@163.com>
2022-02-04 15:48:03 +0800

    【middle】二叉树：257. 二叉树的所有路径

---
+++ src/main/java/first/tree/t257.java
@@ -0,0 +1,30 @@
+package first.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class t257 {
+    List<String> res = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        dfs(root, new StringBuilder());
+        return res;
+    }
+
+    private void dfs(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            return;
+        }
+        sb.append(root.val);
+        if (root.left == null && root.right == null) {
+            res.add(sb.toString());
+            return ;
+        }
+        sb.append("->");
+        dfs(root.left, sb);
+        dfs(root.right, sb);
+    }
+}
