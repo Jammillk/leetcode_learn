4208271ff5001b69a9cb6ce5fa40fa4b965633df
tanjiaming99 <tanjiaming99@163.com>
2022-02-12 13:21:30 +0800

    【middle】二叉树：235. 二叉搜索树的最近公共祖先

---
+++ src/main/java/first/tree/t235.java
@@ -0,0 +1,29 @@
+package first.tree;
+
+public class t235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }else{
+            return root;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
