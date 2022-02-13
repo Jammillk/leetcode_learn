ec35ac9dadb1e645efe7f04e18c8bac676c69c1c
tanjiaming99 <tanjiaming99@163.com>
2022-02-14 00:34:33 +0800

    【middle】回溯：77. 组合
    选/不选

--- src/main/java/first/backtrack/t77.java
+++ src/main/java/first/backtrack/t77.java
@@ -4,6 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class t77 {
+    private void ddfs(int n, int k, int begin, List<Integer> list){
+        if (k  == 0){
+            res.add(new ArrayList<>(list));
+            return ;
+        }
+        if (begin > n - k + 1){
+            return ;
+        }
+        // 不选
+        ddfs(n, k , begin + 1, list);
+
+        // 选
+        list.add(begin);
+        ddfs(n, k - 1, begin + 1, list);
+        list.remove(list.size() - 1);
+    }
+
+
+
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
         dfs(n, k, 1, new ArrayList<>());
