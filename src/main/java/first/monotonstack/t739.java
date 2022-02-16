d139f0a533e75c5101d0c548b291dd53e31ea741
tanjiaming99 <tanjiaming99@163.com>
2022-02-16 12:32:22 +0800

    【middle】单调栈：739. 每日温度

---
+++ src/main/java/first/monotonstack/t739.java
@@ -0,0 +1,26 @@
+package first.monotonstack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class t739 {
+    public static void main(String[] args) {
+        t739 obj = new t739();
+        obj.dailyTemperatures(new int[]{
+                73, 74, 75, 71, 69, 72, 76, 73
+        });
+    }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int preIndex = stack.pop();
+                res[preIndex] = i - preIndex;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
