cea9510cd8e6ebdb2ca6634e2049a903c44d72c7
tanjiaming99 <tanjiaming99@163.com>
2022-02-16 12:49:27 +0800

    【easy】单调栈：496. 下一个更大元素 I

---
+++ src/main/java/first/monotonstack/t496.java
@@ -0,0 +1,30 @@
+package first.monotonstack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class t496 {
+    public static void main(String[] args) {
+        t496 obj = new t496();
+        obj.nextGreaterElement(new int[]{4,1,2}, new int[]{4,1,2});
+    }
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < len2; i++) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        int[] res = new int[len1];
+        for (int i = 0; i < len1; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
