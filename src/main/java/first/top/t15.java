120770e4cafd7078d45ec7998fd153dda6be5894
tanjiaming99 <tanjiaming99@163.com>
2022-02-20 23:26:22 +0800

    15. 三数之和

---
+++ src/main/java/first/top/t15.java
@@ -0,0 +1,43 @@
+package first.top;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class t15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left + 1 < nums.length && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (right - 1 >= 0 && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+}
